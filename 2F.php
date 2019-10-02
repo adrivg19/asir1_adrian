8fcaafd6427140fcf5d92cb33834ac39aca18462
adrivg19 <adrianvillargarcia126@gmail.com>
2019-10-02 09:20:47 +0200

    Array

---
+++ 2F.php
@@ -0,0 +1,44 @@
+<?php
+$a = [
+	'Enero',
+	'Febrero',
+	'Marzo',
+	'Abril',
+	'Mayo',
+	'Junio',
+	'Julio',
+	'Agosto',
+	'Septiembre',
+	'Octubre',
+	'Noviembre',
+	'Diciembre',
+];
+function mes($n) {
+	$a = [
+	'Enero',
+	'Febrero',
+	'Marzo',
+	'Abril',
+	'Mayo',
+	'Junio',
+	'Julio',
+	'Agosto',
+	'Septiembre',
+	'Octubre',
+	'Noviembre',
+	'Diciembre',
+	];
+return $a[$n-1];
+};
+echo mes(1)."<br>";
+echo mes(2)."<br>";
+echo mes(4)."<br>";
+echo mes(5)."<br>";
+
+	
+for($i=0;$i<12;$i++)
+echo $a[$i]."<br>";
+foreach($a as $mes)
+echo($mes)."<br>";
+
+	
