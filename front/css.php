b585c0179e8648cce317081028b60742b1f07b80
adrivg19 <adrianvillargarcia126@gmail.com>
2019-10-21 10:51:49 +0200

    Css: Atributo Style inline

---
+++ front/css.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+		<title>Trabajo HTML</title>
+	</head>
+	
+	<body style="background-color:blue;">
+		<center>
+		<table style="
+			background-color:yellow;
+			margin: 0px 100px;
+			padding: 100px;			
+			"><tr><td>
+		<h1>Practica de html</h1>
+			<h2 style="text-decoration:underline;">Prueba 1</h2>
+				<h3 style="font-weight: 1000;">Prueba 2</h3>
+					<h4>Prueba 3</h4>
+		<img src="Gumball.gif" width="120" height="100"/>
+		<form>
+			<input/>
+			<input type="submit" value="Enviar">
+		</form>
+		<h3>Autonomias Bañadas por el cantabrico</h3>
+		<table border=1>
+			<tr><td>Autonomias</td><td>Provincia</td></tr>
+			<tr><td>Galicia</td><td>A Coruña</td></tr>
+			<tr><td>Galicia</td><td>Lugo</td></tr>
+			<tr><td>Galicia</td><td>Orense</td></tr>
+			<tr><td>Galicia</td><td>Pontevedra</td></tr>
+			<tr><td>Asturias</td><td>Asturias</td></tr>
+			<tr><td>Cantabria</td><td>Cantabria</td></tr>
+			<tr><td>Pais Vasco</td><td>Guipuzcoa</td></tr>
+			<tr><td>Pais Vasco</td><td>Vizcaya</td></tr>
+			<tr><td>Pais Vasco</td><td>Alava</td></tr>
+			
+		</table>
+		<span style="
+			color:aquamarine;
+			font-size:20pt;
+		"> Hola Buenas Tardes</span>
+		</td></tr></table>
+		</center>
+			
+	</body>
+</html>
---
+++ html.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Trabajo HTML</title>
+</head>
+<body>
+<h1>Practica de html</h1>
+	<h2>Prueba 1</h2>
+		<h3>Prueba 2</h3>
+			<h4>Prueba 3</h4>
+			
+			eoeoeoeo
+<img src="Gumball.gif" width="50"/>
+
+</body>
+</html>
